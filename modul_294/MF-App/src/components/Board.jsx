62d463cb9ac63d61054ad276c43ffa5aa25294e5
mykhaylo zhovkevych <148889468+mykhaylo-zhovkevych@users.noreply.github.com>
2024-09-10 23:29:30 +0200

    commit

---
+++ modul_294/MF-App/src/components/Board.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+function Board() {
+  return (
+    
+    <div className="container">
+   
+   <aside className="sidebar">
+      halooo
+      </aside>
+      
+    <main className="board">
+      halloo
+
+    </main>
+    
+  </div>
+
+
+  );
+}
+
+export default Board;
---
+++ modul_294/MF-App/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <h1>Besser als JIRI, weil es Open-Source ist</h1>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
