37bb1adb8788d06eee41ce90d03402d2dcc96682
mykhaylo zhovkevych <148889468+mykhaylo-zhovkevych@users.noreply.github.com>
2024-09-13 22:59:10 +0200

    commit

--- modul_294/MF-App/src/components/Header.jsx
+++ modul_294/MF-App/src/components/Header.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
-
 function Header() {
     return (
         <header>
             <nav>
                 <h1>Besser als JIRI, weil es Open-Source ist</h1>
-                
             </nav>
         </header>
     );
--- modul_294/MF-App/src/components/Statistic.jsx
+++ modul_294/MF-App/src/components/Statistic.jsx
@@ -1,10 +1,65 @@
-function Statistic() {
-    return (
-        <div>
-            
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 
-        </div>
-    );
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function Statistic() {
+  const [tasks, setTasks] = useState([]);
+  
+  const [chartData, setChartData] = useState({
+    labels: ['To Do', 'In Progress', 'Done'],
+    datasets: [
+      {
+        label: 'Task Status',
+        data: [0, 0, 0], 
+      },
+    ],
+  });
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/projektarbeits/documents');
+        const tasksData = response.data;
+        console.log(tasksData);
+
+        // Berechnung der Statistiken
+        const toDoCount = tasksData.filter(task => task.content.status === 'To Do').length;
+        const inProgressCount = tasksData.filter(task => task.content.status === 'In Progress').length;
+        const doneCount = tasksData.filter(task => task.content.status === 'Done').length;
+
+        // Aktualisiere die Chart-Daten
+        setChartData({
+          labels: ['To Do', 'In Progress', 'Done'],
+          datasets: [
+            {
+              label: 'Task Status',
+              // Aktualisierte Werte
+              data: [toDoCount, inProgressCount, doneCount],
+              backgroundColor: ['#c0c0c0', '#007bff', '#00000'],
+              hoverOffset: 4,
+            },
+          ],
+        });
+
+        setTasks(tasksData);
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      }
+    };
+    fetchTasks();
+  }, []);
+
+  return (
+    <div className="statistics">
+      <h1>Task Statistics</h1>
+      <div className="chart-container">
+        <Pie data={chartData} />
+      </div>
+    </div>
+  );
 }
 
 export default Statistic;
